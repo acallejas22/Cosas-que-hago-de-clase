909d72eeeec0d4aaa6811a2c7f4fe423bde21300
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2022-09-20 00:17:20 +0200

    Update ej4.java

--- MP3/ejerciciosjava/ej4.java
+++ MP3/ejerciciosjava/ej4.java
@@ -1,7 +1,7 @@
 public class Main {
   public static void main(String[] args) {
     int a =-5+8*6;
-    int b =(55+9)/9;
+    int b =(55+9)%9;
     int c =20+ (-3*5)/8;
     int d =5+15/3*2-8/3;
    System.out.println(a);
