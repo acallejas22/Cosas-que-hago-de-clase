abcbe44706f9f0790ae697e0dca23cb61fa85ad9
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2022-09-19 20:55:44 +0200

    Create ej3.java

---
+++ MP3/ejerciciosjava/ej3.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[]args){
+  
+     int div = 50/3;
+   //float div = 50/3; esto es para declarar una variable que es decimal
+   System.out.println(div);
+}
+}
