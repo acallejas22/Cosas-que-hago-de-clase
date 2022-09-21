8064f0180aec26bab19d386dcecaeba40aaf2bae
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2022-09-21 03:10:11 +0200

    Create ej7.java

---
+++ MP3/ejerciciosjava/ej7.java
@@ -0,0 +1,11 @@
+public  class ForLoops {
+    public  static void main(String[] args){
+        int b = 8;
+        
+        for(int i = 0; i< 11; i++){
+            
+            System.out.println(b + "x" + i + "=" + i*b);
+            
+        }
+    }
+}
