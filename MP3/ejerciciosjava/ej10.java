43949c3100b62fe72599beecdca6ea7ab6c6b48a
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2022-10-02 02:54:26 +0200

    Create ej10.java

---
+++ MP3/ejerciciosjava/ej10.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+    double a =(float) 4.0 * (1 - (1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
+    
+   System.out.println(a);
+   
+  }
+}
