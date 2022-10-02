fd03e9202d6e1b981803aee72822aa68bb76d503
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2022-10-02 02:14:05 +0200

    Create ej8.java

---
+++ MP3/ejerciciosjava/ej8.java
@@ -0,0 +1,12 @@
+public class LoopsExample {
+    
+    public  static void main(String args[]){
+        
+        
+        
+        System.out.println("   j     a    v     v   a");
+        System.out.println("   j    a a    v   v   a a");  
+        System.out.println("j  j   aaaaa    V V   aaaaa "); 
+        System.out.println(" jj   a     a    V   a     a "); 
+    }
+}
