c1ab7ba0a5382289bdb4b670270f68a0d7c1fa92
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2022-11-10 19:55:17 +0100

    Update secuencia.py

--- MP14/biopython/secuencia.py
+++ MP14/biopython/secuencia.py
@@ -1,19 +1,26 @@
 from Bio import SeqIO
+#from Bio import Entrez
 
 
-for seq_record in SeqIO.parse("sequence3.fasta","fasta"):
+for seq_record in SeqIO.parse("sequence3.fasta",  "fasta"):
     print(seq_record + "\n")
 
-    print("id: " + seq_record.id + "\n")
-    print("name: " + seq_record.name + "\n")
-    print("description: " + seq_record.description + "\n")
-    print("seq: " + seq_record.seq + "\n")
-
+    #print("id: " + seq_record.id + "\n")
+    #print("name: " + seq_record.name + "\n")
+    #print("description: " + seq_record.description + "\n")
+    #print("seq: " + seq_record.seq + "\n")
+    print(seq_record.seq)
+    complementaria = seq_record.seq.complement()
+    print(complementaria)
 
     counterA = 0
     counterT = 0
     counterC = 0
     counterG = 0
+    countera = 0
+    countert = 0
+    counterc = 0
+    counterg = 0
     for i in seq_record.seq:
         if i == "A":
             counterA +=1
@@ -23,7 +30,48 @@ for seq_record in SeqIO.parse("sequence3.fasta","fasta"):
             counterT +=1
         if i == "G":
             counterG +=1
+
     print(counterA)
     print(counterC)
     print(counterG)
     print(counterT)
+    print("-------------------------------")
+    for i in complementaria:
+
+        if i == "A":
+            countera +=1
+        if i == "C":
+            counterc +=1
+        if i == "T":
+            countert +=1
+        if i == "G":
+            counterg +=1
+    print(countera)
+    print(counterc)
+    print(counterg)
+    print(countert)
+
+
+
+
+
+if counterA == countert:
+    print("Todo bien en A y t")
+elif counterT == countera:
+        print("T y a tan bien")
+else:
+    print("T y a y A y t tan mal")
+
+if counterC == counterg:
+    print("Todo bien en C y g")
+elif counterG == counterc:
+        print("G y c tan bien")
+else:
+    print("G y c y C y g tan mal")
+
+
+
+
+
+
+
