c35b6aed559b45a4eb8de0f9b0f15115a37bed43
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2023-02-13 19:30:48 +0100

    Create invertir.java

---
+++ MP3/ejerciciosjava/recursivo/invertir.java
@@ -0,0 +1,20 @@
+import java.utils;
+
+public class java{
+    public static void main(String[]args){
+        number = 6;
+    }
+
+    int factorial(number){
+        
+        if(number >= 1){
+            int result= factoial(number -1);
+            result = number + result;
+            return result;
+        }else{
+            return 1;
+        }
+        return c;
+
+    }
+}
