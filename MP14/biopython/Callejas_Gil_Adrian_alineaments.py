5708832d363b66b6282ed4bc6a334e530c9911b1
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2023-03-20 15:25:57 +0100

    Add files via upload

---
+++ MP14/biopython/Callejas_Gil_Adrian_alineaments.py
@@ -0,0 +1,55 @@
+import Bio
+from Bio.Seq import Seq
+from Bio import SeqIO
+
+
+
+def alinea(cadena1,cadena2):
+    valor = 0
+    for i in range(len(cadena1)):
+        if cadena1[i] == cadena2[i]:
+            valor += 1
+        else:
+            valor -= 1
+    return valor
+
+def trencaid(identificador):
+    vector = identificador.split("|")
+    return vector[3]
+
+
+## en el diccionari creem les parelles claus valors tals que, cada clau és l'identificador, prèviament
+## tractat, i cada valor són els 200 primers nucleòtids de cada seqüència
+
+diccionari = {}
+
+for i in SeqIO.parse("ls_orchid.fasta","fasta"):
+    valor = trencaid(i.id) #en valor vull només una part del id
+    diccionari[valor] = i.seq[:200]
+
+# a màxim guardarem la màxima puntuació d'una alineació i a cadenammaxim1 i cadenamaxim2 guardarem
+## els identificadors de les dues cadenes que tene el màxim alineament
+
+maxim = 0
+cadenamaxim1 = ""
+cadenamaxim2 = ""
+## per crear el document heu de recórrer totes les claus del diccionari i alinear el seu valor amb 
+## tots els altres valors
+## haureu també de guardar
+
+claus = list(diccionari.keys())
+
+with open("informe.txt", "w") as fitxer:
+    for i in range(len(claus)):
+        fitxer.write(" treballem amb " + claus[i] + "\n")
+        fitxer.write("---------------------------\n")
+        for j in range(i + 1, len(claus)):
+            valor = alinea(diccionari[claus[i]], diccionari[claus[j]])
+            fitxer.write(str(valor) + " punts amb " + claus[j] + ": \n")
+            if valor <= maxim:
+                maxim = valor
+                cadenamaxim1 = claus[i]
+                cadenamaxim2 = claus[j]
+        fitxer.write("---------------------------\n")
+    fitxer.write("EL MILLOR ALINEAMENT ES ENTRE: " + cadenamaxim1 + " i " + cadenamaxim2 + "\n")
+    fitxer.write("EL MILLOR ALINEAMENT TE: " + str(maxim) + "punts" + "\n")
