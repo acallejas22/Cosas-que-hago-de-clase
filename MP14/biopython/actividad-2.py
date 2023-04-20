7222fecf989b36b5f21ae714ef77cf02ef481a9a
acallejas22 <113199532+acallejas22@users.noreply.github.com>
2023-04-20 15:59:41 +0200

    Update actividad-2.py

--- MP14/biopython/actividad-2.py
+++ MP14/biopython/actividad-2.py
@@ -2,22 +2,33 @@ from Bio import SeqIO
 from Bio.Seq import Seq
 from Bio import Entrez
 
-Entrez.email='acallejas22@ilg.cat'
+Entrez.email = 'acallejas22@ilg.cat'
 
-
-
-
-handle=Entrez.efetch(db="nucleotide", id="NM_000900.5", rettype="fasta", retmode="text")
-handle2=Entrez.efetch(db="nucleotide", id="NM_000900.5", rettype="gb", retmode="text")
 """----------El código no está terminado---------"""
+with open("NM_000900.5.fasta", "w") as fitxer:
+    handle = Entrez.efetch(db="nucleotide", id="NM_000900.5", rettype="fasta", retmode="text")
+    fitxer.write(handle.read())
 
-with open("fitxerfasta.fasta", "w") as resultats:
-        resultats.write(handle.read())
+with open("resultat.txt", "w") as fitxer:
+    with open("NM_000900.5.fasta") as handle:
+        for rec in SeqIO.parse(handle, "fasta"):
+            fitxer.write(rec.id + "\n")
+            fitxer.write(rec.description + "\n")
+            fitxer.write(str(rec.seq))
+            fitxer.write(str(rec.seq)[0:50])
+            print(rec)
+
+with open("NM_000900.5.gb", "w") as fitxer:
+    handle = Entrez.efetch(db="nucleotide", id="NM_000900.5", rettype="gb", retmode="text")
+    fitxer.write(handle.read())
+
+with open("resultatgb.txt", "w") as fitxer:
+    with open("NM_000900.5.gb") as fitxergb:
+        fitxer.write(rec.id)
 
 
-
-with open("fitxerfasta.gb", "w") as resultats2:
+"""with open("fitxerfasta.gb", "w") as resultats2:
         resultats2.write(handle2.read())
-        with open("fitxerfasta.txt", "w") as resultats3:
-                for rec in SeqIO.parse(handle2, "genbank"):
-                        resultats3.write(rec.id)
+with open("fitxerfasta.txt", "w") as resultats3:
+        for rec in SeqIO.parse(handle2, "genbank"):
+             resultats3.write(rec.id)"""
